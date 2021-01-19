9b1dff0539049e4d9e30eb24b374c870d43e3688
Adisonpulun <teelek.adison@gmail.com>
2021-01-19 18:42:41 +0700

    add gitignore

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 8080;
+
 
 app.get("/" , (req ,res) => {
     res.json({result: "ok" , data:["hello"]})
@@ -8,4 +10,5 @@ app.get("/" , (req ,res) => {
 
 app.listen(PORT , ()=>{
     console.log(`Server is running . ${PORT}`)
+    console.log(process.env.CLEARDB_DATABASE_URL);
 })
